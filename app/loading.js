f6c9b8c5defa7bdfd8792cd4da2a605a7135457d
vaibhavM <vaibhav@kali.kali>
2025-05-19 17:41:44 +0530

    add meals.db, loading page, meals page data from database.

---
+++ app/loading.js
@@ -0,0 +1,6 @@
+import classes from './loading.module.css'
+const MealsLoadingPage = () => {
+    return <p className={classes.loading}>Fetching Meals...</p>
+}
+
+export default MealsLoadingPage;
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,24 @@
-
-const MealsPage = () => {
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+// Yes next server components can be async..............
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Meals
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>Delicious meals, created{' '} <span className={classes.highlight}>by you</span></h1>
+        <p>Choose your favourite recipie and cook it yourself, It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals}/>
+      </main>
+    </>
+    
   )
 }
 
